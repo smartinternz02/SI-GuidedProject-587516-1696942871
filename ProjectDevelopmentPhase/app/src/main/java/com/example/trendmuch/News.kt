adab3308691fa0b32936757739a92c987ff8f481
gautam <gautammhiremath@gmail.com>
2023-11-03 23:19:39 +0530

    Development Phase initial commit

---
+++ ProjectDevelopmentPhase/app/src/main/java/com/example/trendmuch/News.kt
@@ -0,0 +1,13 @@
+package com.example.trendmuch
+
+
+import com.google.gson.annotations.SerializedName
+
+
+data class News (
+
+    @SerializedName("status"       ) var status       : String?             = null,
+    @SerializedName("totalResults" ) var totalResults : Int?                = null,
+    @SerializedName("articles"     ) var articles     : ArrayList<Articles> = arrayListOf()
+
+)
